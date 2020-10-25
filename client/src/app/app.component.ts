8bc72d3498d6480d2d7b0351d1cc4ea02585502d
Che Wan Muhammad Amiradzim <amir.adzim@gmail.com>
2020-10-26 04:21:46 +0800

    26.10.2020v2

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Material Take-Off';
+  users: any;
+  entries: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+    this.getEntries();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  getEntries() {
+    this.http.get('https://localhost:5001/api/entries').subscribe(response => {
+      this.entries = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
