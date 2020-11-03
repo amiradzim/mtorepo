ada75604090385899a76b486b23b98b05ea8d3d4
Che Wan Muhammad Amiradzim <amir.adzim@gmail.com>
2020-11-03 12:02:23 +0800

    11.3.2020v1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component ({
   selector: 'app-root',
@@ -11,9 +13,23 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Material Take-Off';
   users: any;
-  entries: any;
-
+  entries: MatTableDataSource<any>;
+  displayedColumns: string[] = ['id', 'projName', 'platNo', 'costElement', 'subCostElement',
+'section', 'matType', 'matVariant', 'procMethod', 'dwgNo', 'dwgCode', 'matGroup', 'description',
+'diameter', 'thickness', 'nal', 'unitWeight', 'baseWeight', 'surfaceArea'];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  
   constructor(private http: HttpClient) {}
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.entries.filter = filterValue.trim().toLowerCase();
+
+    if (this.entries.paginator) {
+      this.entries.paginator.firstPage();
+    }
+  }
 
   ngOnInit() {
     this.getUsers();
@@ -29,10 +45,13 @@ export class AppComponent implements OnInit {
   }
 
   getEntries() {
-    this.http.get('https://localhost:5001/api/entries').subscribe(response => {
-      this.entries = response;
+    this.http.get('https://localhost:5001/api/entries').subscribe((response: any[]) => {
+      this.entries = new MatTableDataSource(response);
+      this.entries.sort = this.sort;
+      this.entries.paginator = this.paginator;
     }, error => {
       console.log(error);
     })
   }
 }
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [
@@ -18,9 +23,15 @@ import { MatSliderModule } from '@angular/material/slider';
     HttpClientModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
